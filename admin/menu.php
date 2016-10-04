b416e3c2f004f67363fbf841047a656fab1208f9
Unknown <swt02026@gmail.com>
2016-10-04 15:25:28 +0800

    change icon

---
+++ admin/menu.php
@@ -0,0 +1,24 @@
+<?php
+$adminmenu = array();
+$icon_dir=substr(XOOPS_VERSION,6,3)=='2.6'?"":"images/";
+
+$i = 1;
+$adminmenu[$i]['title'] = "demo home page";
+$adminmenu[$i]['link'] = 'admin/index.php' ;
+$adminmenu[$i]['desc'] = "";
+$adminmenu[$i]['icon'] = 'images/admin/home.png' ;
+
+
+$i++;
+$adminmenu[$i]['title'] = 'scratch demo';
+$adminmenu[$i]['link'] = "admin/main.php";
+$adminmenu[$i]['desc'] = 'scratch demo 介面' ;
+$adminmenu[$i]['icon'] = "images/admin/home.png";
+
+
+$i++;
+$adminmenu[$i]['title'] = "about";
+$adminmenu[$i]['link'] = 'admin/about.php';
+$adminmenu[$i]['desc'] = "";
+$adminmenu[$i]['icon'] = 'images/admin/about.png';
+?>
