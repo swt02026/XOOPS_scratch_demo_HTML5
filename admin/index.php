70579e5cb927e8047ebabaeaffd5445b4f38efdc
Unknown <swt02026@gmail.com>
2016-10-04 17:01:36 +0800

    change file
     index.html

--- admin/index.php
+++ admin/index.php
@@ -1,6 +1,9 @@
 <?php 
 	include 'header.php';
 	include_once '../function.php';
-	echo "string";
+	include_once XOOPS_ROOT_PATH."/header.php";
+	global xoopsTpl;
+	xoopsTpl.assign('all_data', $all_data);
+	
 	include 'footer.php';
 ?>
