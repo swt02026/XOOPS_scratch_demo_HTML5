2c881f046cfe5b07db57829ef7369c0d1de765cc
Unknown <swt02026@gmail.com>
2016-10-05 07:56:41 +0800

    change file
     index.php

--- admin/index.php
+++ admin/index.php
@@ -1,10 +1,19 @@
 <?php 
-	include 'header.php';
-	include_once '../function.php';
 	$xoopsOption['template_main'] = "scratch_demo_index.html";
+	include 'header.php';
+	include_once "../function.php";
 	include_once XOOPS_ROOT_PATH."/header.php";
-	//global $xoopsTpl;
-	//$xoopsTpl->assign('all_data', $all_data);
+	$all_data=[
+		[
+			"link" => "google.com",
+			"class_num" => 1
+		],
+		[
+			"link" => "google2.com",
+			"class_num" => 2
+		]
+	];
+	$xoopsTpl->assign('all_data', $all_data);
 	
 	include 'footer.php';
 ?>
