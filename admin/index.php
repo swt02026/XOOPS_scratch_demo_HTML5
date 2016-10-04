cb2566609d4734341d82344dc2b00ac600cda4dc
Unknown <swt02026@gmail.com>
2016-10-04 16:59:19 +0800

    change file
     index.html

--- admin/index.php
+++ admin/index.php
@@ -1,19 +1,6 @@
-<?php
+<?php 
 	include 'header.php';
-	//$xoopsOption['template_main']="scratch_demo_index.html"
-	//include_once XOOPS_ROOT_PATH.'/header.php';
-	// global $xoospTpl;
-	// $all_data = [
-	// 	[
-	// 		"link" => "http://google.com",
-	// 		"class_num" => "ma12"
-	// 	],
-	// 	[
-	// 		"link" => "http://google2.com",
-	// 		"class_num" => "ma13"
-	// 	]
-	// ];
-	// $xoopsTpl->assign("all_data", $all_data);
-	echo "aaa";
+	include_once '../function.php';
+	echo "string";
 	include 'footer.php';
 ?>
