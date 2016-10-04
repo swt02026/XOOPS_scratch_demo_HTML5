eac071c816acf1161e87a124acc7c1f5a8a4daef
Unknown <swt02026@gmail.com>
2016-10-05 07:40:38 +0800

    change file
     index.php

--- admin/index.php
+++ admin/index.php
@@ -1,9 +1,10 @@
 <?php 
 	include 'header.php';
 	include_once '../function.php';
-	include_once XOOPS_ROOT_PATH."/header.php";
-	global $xoopsTpl;
-	$xoopsTpl->assign('all_data', $all_data);
+	$xoopsOption['template_main'] = "scratch_demo_index.html";
+	//include_once XOOPS_ROOT_PATH."/header.php";
+	//global $xoopsTpl;
+	//$xoopsTpl->assign('all_data', $all_data);
 	
 	include 'footer.php';
 ?>
