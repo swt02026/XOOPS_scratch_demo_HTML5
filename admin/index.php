747aad48e9bc3b44df03f6f027e56873980f9c0b
Unknown <swt02026@gmail.com>
2016-10-05 08:26:07 +0800

    change file
     index.php

--- admin/index.php
+++ admin/index.php
@@ -1,5 +1,5 @@
 <?php 
-	include '../../mainfile.php';
+	include '../../../mainfile.php';
 	$xoopsOption['template_main'] = "scratch_demo_index.html";
 	include XOOPS_ROOT_PATH."/header.php";
 	$all_data=[
