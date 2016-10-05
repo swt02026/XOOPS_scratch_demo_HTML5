54f86499a5243398df88dbec75bf174a4ccdd50d
Unknown <swt02026@gmail.com>
2016-10-06 07:28:15 +0800

    change file

--- admin/about.php
+++ admin/about.php
@@ -1,8 +1,9 @@
 <?php
-	$xoopsOption['template_main'] = "scratch_demo_about.html"
+	//$xoopsOption['template_main'] = "scratch_demo_about.html"
 	include '../../../include/cp_header.php';
 	xoops_cp_header();
 	$xoopsTpl->assign('data', "hello");
+	$xoopsTpl->display("scratch_demo_about.html");
 	xoops_cp_footer();
 
 ?>
