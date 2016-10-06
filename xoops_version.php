cd94507706caacad7495deee913c1f2b1f6b91df
Unknown <swt02026@gmail.com>
2016-10-06 15:28:03 +0800

    change file
    template dir in admin/about.php

--- xoops_version.php
+++ xoops_version.php
@@ -52,15 +52,16 @@ $modversion['hasMain'] = 1;
 
 
 //---樣板設定---//
-$modversion['templates'] = array();
-$i=0;
-$modversion['templates'][$i]['file'] = 'scratch_demo_adm_main.html';
-$modversion['templates'][$i]['description'] = 'school_news_adm_main.html';
-
-$i++;
-$modversion['templates'][$i]['file'] = 'scratch_demo_index.html';
-$modversion['templates'][$i]['description'] = 'scratch_demo_index.html';
-
+$modversion['templates'] = [
+	[
+		'file' => 'scratch_demo_about.html',
+		'description' => 'scratch_demo_about.html'
+	],
+	[
+		'file' => 'scratch_demo_index.html',
+		'description' => 'scratch_demo_index.html'
+	]	
+];
 
 //---搜尋---//
 //$modversion['hasSearch'] = 1;
