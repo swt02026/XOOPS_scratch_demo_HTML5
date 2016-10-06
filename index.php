e396c27af7d2ec4696c4fb9cc263369b84bcadb7
Unknown <swt02026@gmail.com>
2016-10-06 13:35:56 +0800

    change file
    template dir in admin/about.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php 
 	include '../../mainfile.php';
-	$xoopsOption['template_main'] = "scratch_demo_index.html";
+	$xoopsOption['template_main'] = "scratch_demo_about.html";
 	include XOOPS_ROOT_PATH."/header.php";
 	$all_data=[
 		[
