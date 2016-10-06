845b0e13874e3f90b5b3b0473b307716c95dbcaf
Unknown <swt02026@gmail.com>
2016-10-06 11:02:30 +0800

    change file
    template dir in admin/about.php

--- admin/about.php
+++ admin/about.php
@@ -3,8 +3,9 @@
 	include '../../../include/cp_header.php';
 	xoops_cp_header();
 	echo "HELOO";
-	$xoopsTpl->assign('data',"dddd");
-	$o=$xoopsTpl->fetch('../templates/scratch_demo_about.html');
-	echo $o;
+	print_r($xoopsTpl);
+	// $xoopsTpl->assign('data',"dddd");
+	// $o=$xoopsTpl->fetch('../templates/scratch_demo_about.html');
+	// echo $o;
 	xoops_cp_footer();
 ?>
