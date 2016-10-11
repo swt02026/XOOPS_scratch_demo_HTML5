8c3ae63f1a87713047b565f986178d14df7bea3a
Unknown <swt02026@gmail.com>
2016-10-11 15:57:19 +0800

    change file

--- get_sb2.php
+++ get_sb2.php
@@ -1,10 +1,6 @@
 <?php 
-<<<<<<< HEAD
 	if (isset($_GET['filename']) 
 		&& preg_match('/[^\n\\\/]/',$str = htmlspecialchars($_GET['filename']))<=0 ) {
-=======
-	if (isset($_GET['filename']) && $str = htmlspecialchars($_GET['filename'])) {
->>>>>>> test
 		echo $str;
 
 	}
