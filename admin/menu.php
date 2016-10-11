18a50c140eb94661f23abc475fbc34b8fd0bd05f
Unknown <swt02026@gmail.com>
2016-10-11 14:39:18 +0800

    change menu.php

--- admin/menu.php
+++ admin/menu.php
@@ -2,15 +2,18 @@
 $adminmenu = array();
 $icon_dir=substr(XOOPS_VERSION,6,3)=='2.6'?"":"images/";
 
-$i = 1;
-$adminmenu[$i]['title'] = "demo home page";
-$adminmenu[$i]['link'] = 'admin/index.php' ;
-$adminmenu[$i]['desc'] = "";
-$adminmenu[$i]['icon'] = 'images/admin/home.png' ;
-
-$i++;
-$adminmenu[$i]['title'] = "about";
-$adminmenu[$i]['link'] = 'admin/about.php';
-$adminmenu[$i]['desc'] = "";
-$adminmenu[$i]['icon'] = 'images/admin/about.png';
+$adminmenu=[
+	[
+		'title' => "demo home page",
+		'link' =>  'admin/index.php',
+		'desc' => "",
+		'icon' => 'images/admin/home.png' 
+	],
+	[
+		'title' => "about",
+		'link' =>  'admin/about.php',
+		'desc' => "",
+		'icon' => 'images/admin/about.png' 
+	]
+];
 ?>
