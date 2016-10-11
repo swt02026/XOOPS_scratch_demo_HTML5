8096299a3a23330335430a275f5097246a79fb51
Unknown <swt02026@gmail.com>
2016-10-11 16:04:28 +0800

    change file

--- get_sb2.php
+++ get_sb2.php
@@ -1,6 +1,6 @@
 <?php 
 	if (isset($_GET['filename']) 
-		&& $str = preg_place('/[\n\/\\]+/','',htmlspecialchars($_GET['filename'])) ) {
+		&& $str = preg_replace('/[\n\/\\]+/','',htmlspecialchars($_GET['filename'])) ) {
 		echo $str;
 
 	}
