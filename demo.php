2f50b7bcf5699d2cc3e89ee5f198421443b9d7e9
Unknown <swt02026@gmail.com>
2016-10-12 16:44:05 +0800

    change file

--- demo.php
+++ demo.php
@@ -2,7 +2,7 @@
 	include '../../mainfile.php';
 	$xoopsOption['template_main'] = "scratch_demo_demo.html";
 	include XOOPS_ROOT_PATH."/header.php";
-
+	
 	
 	include_once XOOPS_ROOT_PATH.'/footer.php';
 ?>
--- index.php
+++ index.php
@@ -3,6 +3,5 @@
 	$xoopsOption['template_main'] = "scratch_demo_demo.html";
 	include XOOPS_ROOT_PATH."/header.php";
 
-	echo $modversion['templates'];
 	include_once XOOPS_ROOT_PATH.'/footer.php';
 ?>
