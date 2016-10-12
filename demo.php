283cef7a6b0f49b6d91ef5ad8115bd1686500db1
Unknown <swt02026@gmail.com>
2016-10-12 16:26:11 +0800

    add file

---
+++ demo.php
@@ -0,0 +1,8 @@
+<?php 
+	include '../../mainfile.php';
+	$xoopsOption['template_main'] = "scratch_demo_demo.html";
+	include XOOPS_ROOT_PATH."/header.php";
+
+	
+	include_once XOOPS_ROOT_PATH.'/footer.php';
+?>
