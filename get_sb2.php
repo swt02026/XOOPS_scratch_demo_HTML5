d0d29b1eb19ef802f81ca10b45ea690e45260d30
Unknown <swt02026@gmail.com>
2016-10-13 08:25:20 +0800

    content type filename

--- get_sb2.php
+++ get_sb2.php
@@ -2,6 +2,7 @@
 	if (isset($_GET['filename']) 
 		&& $str = addslashes(htmlspecialchars($_GET['filename'])))  {
 		header("Content-Type:application/octet-stream");
+		header('Content-Disposition: attachment; filename="'.$str.'"');
 		readfile('sb2-js/examples/'.$str);
 	}
 ?>
