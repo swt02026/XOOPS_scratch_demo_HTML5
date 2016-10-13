f46bd9f8fc973b092a1acb123e7924679ef816f0
Unknown <swt02026@gmail.com>
2016-10-13 08:15:07 +0800

    change file

--- get_sb2.php
+++ get_sb2.php
@@ -1,6 +1,6 @@
 <?php 
 	if (isset($_GET['filename']) 
-		&& $str = addslashes(htmlspecialchars($_GET['filename'])))  {
+		&& $str = htmlspecialchars($_GET['filename']))  {
 		header("Content-Type:application/octet-stream");
 		echo('sb2-js/examples/'.$str);
 	}
