2eb14f6fd923533425e49d335074c6e6d3c37236
Unknown <swt02026@gmail.com>
2016-10-13 10:42:59 +0800

    change file

--- get_sb2.php
+++ get_sb2.php
@@ -3,6 +3,9 @@
 		&& $str = addslashes(htmlspecialchars($_GET['filename'])))  {
 		header("Content-Type:application/octet-stream");
 		header('Content-Disposition: attachment; filename="'.$str.'"');
+		header('Content-Length:'.filesize('sb2-js/examples/'.$str))
+		header('Accept-Ranges: bytes')
 		readfile('sb2-js/examples/'.$str);
+		exit;
 	}
 ?>
