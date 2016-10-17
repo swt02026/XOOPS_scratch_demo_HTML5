fe1c8ad638cf031d9b0ef46e5f76d0e9173fa619
Unknown <swt02026@gmail.com>
2016-10-17 13:34:46 +0800

    scan origin dir

--- index.php
+++ index.php
@@ -4,8 +4,7 @@
 	include XOOPS_ROOT_PATH."/header.php";
 	$sb2dir = 'sb2-js/examples';
 	$filenames = array_slice(scandir($sb2dir), 2);
-	print_r(scandir('./'));
 	$xoopsTpl->assign('filenames', $filenames);
 
-	//include_once XOOPS_ROOT_PATH.'/footer.php';
+	include_once XOOPS_ROOT_PATH.'/footer.php';
 ?>
