5f4f2aaf38d5b4864492eb3e3a69bbccc0ab28fc
Unknown <swt02026@gmail.com>
2016-10-17 10:37:34 +0800

    change template file

--- index.php
+++ index.php
@@ -2,6 +2,9 @@
 	include '../../mainfile.php';
 	$xoopsOption['template_main'] = "scratch_demo_index.html";
 	include XOOPS_ROOT_PATH."/header.php";
+	$sb2dir = './sb2-js/examples/';
+	$filenames = array_slice(scandir($sb2dir), 2);
+	$xoopsTpl->assign('filenames', $filenames);
 
 	include_once XOOPS_ROOT_PATH.'/footer.php';
 ?>
