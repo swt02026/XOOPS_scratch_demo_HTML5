0f86736aeb4500f71971d82413dda7147db1545a
Unknown <swt02026@gmail.com>
2016-10-24 14:01:25 +0800

    add upload

--- admin/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-	include '../../../include/cp_header.php';
-	xoops_cp_header();
-	
-	xoops_cp_footer();
-
-?>
---
+++ admin/upload.php
@@ -0,0 +1,21 @@
+<?php
+	include '../../../include/cp_header.php';
+	xoops_cp_header();
+	$storage = new \Upload\Storage\FileSystem('../sb2-js/examples/');
+    $file = new \Upload\File('file', $storage);
+
+    $file->addValidations([
+        new \Upload\Validation\Mimetype('application/octet-stream'),
+        new \Upload\Validation\Extension('sb2'),
+        new \Upload\Validation\Size('5M')
+    ]);
+
+    try{
+        $file->upload();
+    }catch (\Exception $e){
+        echo $e->getMessage();
+    }
+
+	xoops_cp_footer();
+
+?>
