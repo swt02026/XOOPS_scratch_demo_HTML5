799b74ab3f0f2c4d14b5a0df29e720542987af1d
Unknown <swt02026@gmail.com>
2016-10-25 10:48:14 +0800

    add filter condition

--- admin/menu.php
+++ admin/menu.php
@@ -4,12 +4,6 @@ $icon_dir=substr(XOOPS_VERSION,6,3)=='2.6'?"":"images/";
 
 $adminmenu=[
 	[
-		'title' => "demo home page",
-		'link' =>  'admin/index.php',
-		'desc' => "",
-		'icon' => 'images/admin/home.png' 
-	],
-	[
 		'title' => "about",
 		'link' =>  'admin/about.php',
 		'desc' => "",
--- src/get_sb2.php
+++ src/get_sb2.php
@@ -1,10 +1,13 @@
 <?php 
 	if (isset($_GET['filename']) 
-		&& $str = addslashes(htmlspecialchars($_GET['filename'])))  {
+		&& $str = filter_var(
+		            addslashes(
+		                htmlspecialchars($_GET['filename']))
+            , FILTER_SANITIZE_STRING))  {
 		header("Content-Type:application/octet-stream");
 		header('Content-Disposition: attachment; filename="'.$str.'"');
 		ob_clean();
-    		flush();
+        flush();
 		readfile('sb2-js/examples/'.$str);
 		exit;
 	}
