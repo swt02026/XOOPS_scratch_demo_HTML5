5a7d045af88f1a6d09e3ea2526839fa098c50a71
Unknown <swt02026@gmail.com>
2016-10-25 11:33:23 +0800

    change get_sb2 path

--- get_sb2.php
+++ get_sb2.php
@@ -5,7 +5,7 @@
 		header('Content-Disposition: attachment; filename="'.$str.'"');
 		ob_clean();
     		flush();
-		readfile('../sb2-js/examples/'.$str);
+		readfile('sb2-js/examples/'.$str);
 		exit;
 	}
 ?>
