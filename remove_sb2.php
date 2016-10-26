272aa664b6c3dace5b2ebd4cd79622865ea9035d
Unknown <swt02026@gmail.com>
2016-10-26 11:42:03 +0800

    change file

--- remove_sb2.php
+++ remove_sb2.php
@@ -7,5 +7,5 @@
     $which_del2 = array_filter($which_del, function ($filename){
         return !file_exists("sb2-js/examples/$filename");
     });
-    print_r($which_del2);
+    print_r($which_del);
 ?>
