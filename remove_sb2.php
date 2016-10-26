cabc26778438b127ead93a1e3fc36cf208103854
Unknown <swt02026@gmail.com>
2016-10-26 11:42:48 +0800

    change file

--- remove_sb2.php
+++ remove_sb2.php
@@ -5,7 +5,7 @@
 
     $which_del = array_map($escape,$_POST['which_del']);
     $which_del2 = array_filter($which_del, function ($filename){
-        return !file_exists("sb2-js/examples/$filename");
+        return file_exists("sb2-js/examples/$filename");
     });
     print_r($which_del);
 ?>
