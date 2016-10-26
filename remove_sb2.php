3a906aa723ac3182d2c06696f169ca009918b47a
Unknown <swt02026@gmail.com>
2016-10-26 11:11:50 +0800

    change file

--- remove_sb2.php
+++ remove_sb2.php
@@ -2,8 +2,8 @@
     $escape = function ($filename){
         echo "aa";
         return htmlspecialchars($filename);
-    }
-    $which_del = implode(',', $_POST['which_del']);
+    };
+    $which_del = explode(',', $_POST['which_del']);
     $del = array_map($escape,$which_del);
     print_r($del);
 ?>
