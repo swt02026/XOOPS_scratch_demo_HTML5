5804231ecec19656a9ed6ee7a05872ae8535879d
Unknown <swt02026@gmail.com>
2016-10-26 08:24:28 +0800

    add manage file list page

--- admin/menu.php
+++ admin/menu.php
@@ -1,20 +1,26 @@
 <?php
-$adminmenu = array();
-$icon_dir=substr(XOOPS_VERSION,6,3)=='2.6'?"":"images/";
+    $adminmenu = array();
+    $icon_dir=substr(XOOPS_VERSION,6,3)=='2.6'?"":"images/";
 
-$adminmenu=[
-	[
-		'title' => "about",
-		'link' =>  'admin/about.php',
-		'desc' => "",
-		'icon' => 'images/admin/about.png' 
-	],
-    [
-        'title' => "上傳作品",
-        'link' =>  'admin/upload.php',
-        'desc' => "",
-        'icon' => 'images/admin/demo.png'
-    ]
+    $adminmenu=[
+        [
+            'title' => "上傳作品",
+            'link' =>  'admin/upload.php',
+            'desc' => "",
+            'icon' => 'images/admin/demo.png'
+        ],
+        [
+            'title' => '作品管理',
+            'link' => '',
+            'desc' => '',
+            'icon' => 'image/admin/demo.png'
 
-];
+        ],
+        [
+            'title' => "about",
+            'link' =>  'admin/about.php',
+            'desc' => "",
+            'icon' => 'images/admin/about.png'
+        ],
+    ];
 ?>
