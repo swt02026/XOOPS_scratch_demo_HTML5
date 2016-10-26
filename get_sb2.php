f6eea3b86289f59fd0a7f067865a518e134457ba
Unknown <swt02026@gmail.com>
2016-10-26 13:23:22 +0800

    change file

--- get_sb2.php
+++ get_sb2.php
@@ -1,11 +1,14 @@
-<?php 
-	if (isset($_GET['filename']) 
+<?php
+    $sb2dir = 'sb2-js/examples/';
+    $filenames = array_slice(scandir($sb2dir), 2);
+	if (isset($_GET['filename'])
+        && in_array($_GET['filename'], $filenames)
 		&& $str = addslashes(htmlspecialchars($_GET['filename'])))  {
 		header("Content-Type:application/zip");
 		header('Content-Disposition: attachment; filename="'.$str.'"');
 		ob_clean();
     		flush();
-		readfile('sb2-js/examples/'.$str);
+		readfile($sb2dir.$str);
 		exit;
 	}
 ?>
