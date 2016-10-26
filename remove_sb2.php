952ff749e6536da80382426710b36fdf16dac7a6
Unknown <swt02026@gmail.com>
2016-10-26 13:27:39 +0800

    change file

--- remove_sb2.php
+++ remove_sb2.php
@@ -2,10 +2,14 @@
     $escape = function ($filename){
         return addslashes(htmlspecialchars($filename));
     };
+    $sb2dir = 'sb2-js/examples/';
+    $filenames = array_slice(scandir($sb2dir), 2);
 
     $which_del = array_map($escape,$_POST['which_del']);
-    $which_del = array_filter($which_del, function ($filename){
-        return file_exists("sb2-js/examples/$filename");
-    });
+    $which_del = array_filter($which_del,
+        function ($filename) use($filenames){
+            return in_array($filename, $filenames);
+        }
+    );
     print_r($which_del);
 ?>
