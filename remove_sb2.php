7570173fa9f76970f55d9b79e8bd0ac466c71a15
Unknown <swt02026@gmail.com>
2016-10-26 11:31:39 +0800

    change file

--- remove_sb2.php
+++ remove_sb2.php
@@ -1,7 +1,11 @@
 <?php
     $escape = function ($filename){
-        return htmlspecialchars($filename);
+        return addslashes(htmlspecialchars($filename));
     };
+
     $which_del = array_map($escape,$_POST['which_del']);
+    $which_del = array_filter($which_del, function ($filename){
+        file_exists("sb2-js/examples/$filename");
+    });
     print_r($which_del);
 ?>
