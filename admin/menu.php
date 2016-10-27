9424ac89bb09de8e21db13285ce9e603c88ba0f4
Unknown <swt02026@gmail.com>
2016-10-27 10:01:29 +0800

    add file

--- admin/menu.php
+++ admin/menu.php
@@ -4,16 +4,22 @@
 
     $adminmenu=[
         [
+            'title' => "作品展示",
+            'link' =>  'index.php',
+            'desc' => "作品展示",
+            'icon' => 'images/admin/demo.jpg'
+        ],
+        [
             'title' => "上傳作品",
             'link' =>  'admin/upload.php',
             'desc' => "上傳作品",
-            'icon' => 'images/admin/demo.png'
+            'icon' => 'images/admin/upload.png'
         ],
         [
             'title' => '作品管理',
             'link' => 'admin/modify_list.php',
             'desc' => '作品管理',
-            'icon' => 'image/admin/demo.png'
+            'icon' => 'image/admin/manage.png'
 
         ],
         [
@@ -21,6 +27,6 @@
             'link' =>  'admin/about.php',
             'desc' => "關於",
             'icon' => 'images/admin/about.png'
-        ],
+        ]
     ];
 ?>
