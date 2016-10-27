5226ed7450fca7f442d42a566bece99061eaf4b7
Unknown <swt02026@gmail.com>
2016-10-27 10:10:34 +0800

    add file

--- admin/menu.php
+++ admin/menu.php
@@ -13,20 +13,20 @@
             'title' => "上傳作品",
             'link' =>  'admin/upload.php',
             'desc' => "上傳作品",
-            'icon' => 'images/admin/upload.png'
+            'icon' => 'images/admin/upload.jpg'
         ],
         [
             'title' => '作品管理',
             'link' => 'admin/modify_list.php',
             'desc' => '作品管理',
-            'icon' => 'image/admin/manage.png'
+            'icon' => 'image/admin/manage.jpg'
 
         ],
         [
             'title' => "關於",
             'link' =>  'admin/about.php',
             'desc' => "關於",
-            'icon' => 'images/admin/about.png'
+            'icon' => 'images/admin/about.jpg'
         ]
     ];
 ?>
