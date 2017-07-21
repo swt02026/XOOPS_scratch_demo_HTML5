c4f821f20b178b667d8569b126ad2a8d0a4df38a
mambax7 <mambax7@gmail.com>
2017-07-21 05:19:17 -0400

    Language files

---
+++ language/schinese_utf8/main.php
@@ -0,0 +1,3 @@
+<?php
+
+define('MD_SCRATCH_GALLERY', "Gallery");
---
+++ language/schinese_utf8/modinfo.php
@@ -0,0 +1,64 @@
+<?php
+/*
+ * You may not change or alter any portion of this comment or credits
+ * of supporting developers from this source code or any supporting source code
+ * which is considered copyrighted (c) material of the original comment or credit authors.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ */
+
+/**
+ * @copyright    XOOPS Project http://xoops.org/
+ * @license      GNU GPL 2 or later (http://www.gnu.org/licenses/gpl-2.0.html)
+ * @package
+ * @since
+ * @author       XOOPS Development Team
+ */
+
+// The name of this module
+define('MI_SCRATCH_NAME', 'Scratch成品展示');
+define('MI_SCRATCH_NAME_DESC', 'Scratch成品展示');
+
+
+//Menu
+define('MI_SCRATCH_ADMENU0', 'Home');
+define('MI_SCRATCH_ADMENU1', '成品展示');
+define('MI_SCRATCH_ADMENU2', '上傳作品');
+define('MI_SCRATCH_ADMENU3', '作品展示');
+define('MI_SCRATCH_ADMENU4', '作品管理');
+define('MI_SCRATCH_ADMENU5', '關於');
+
+define('MI_SCRATCH_ADMENU0_DESC', 'Home');
+define('MI_SCRATCH_ADMENU1_DESC', '成品展示');
+define('MI_SCRATCH_ADMENU2_DESC', '上傳作品');
+define('MI_SCRATCH_ADMENU3_DESC', '作品展示');
+define('MI_SCRATCH_ADMENU4_DESC', '作品管理');
+define('MI_SCRATCH_ADMENU5_DESC', '關於');
+
+
+//Help
+define('MI_SCRATCH_DIRNAME', basename(dirname(dirname(__DIR__))));
+define('MI_SCRATCH_HELP_HEADER', __DIR__ . '/help/helpheader.html');
+define('MI_SCRATCH_BACK_2_ADMIN', 'Back to Administration of ');
+define('MI_SCRATCH_OVERVIEW', 'Overview');
+
+//define('MI_SCRATCH_HELP_DIR', __DIR__);
+
+//help multi-page
+define('MI_SCRATCH_DISCLAIMER', 'Disclaimer');
+define('MI_SCRATCH_LICENSE', 'License');
+define('MI_SCRATCH_SUPPORT', 'Support');
+//define('MI_SCRATCH_REQUIREMENTS', 'Requirements');
+//define('MI_SCRATCH_CREDITS', 'Credits');
+//define('MI_SCRATCH_HOWTO', 'How To');
+//define('MI_SCRATCH_UPDATE', 'Update');
+//define('MI_SCRATCH_INSTALL', 'Install');
+//define('MI_SCRATCH_HISTORY', 'History');
+//define('MI_SCRATCH_HELP1', 'YYYYY');
+//define('MI_SCRATCH_HELP2', 'YYYYY');
+//define('MI_SCRATCH_HELP3', 'YYYYY');
+//define('MI_SCRATCH_HELP4', 'YYYYY');
+//define('MI_SCRATCH_HELP5', 'YYYYY');
+//define('MI_SCRATCH_HELP6', 'YYYYY');
